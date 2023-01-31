024aa3830249cfa35ec8d8917c7e8e566c98af7f
GManTheMarioGuy <123761856+GManTheMarioGuy@users.noreply.github.com>
2023-01-30 19:46:37 -0600

    Update dialogs.h
    Dialog Change

--- text/us/dialogs.h
+++ text/us/dialogs.h
@@ -17,7 +17,7 @@
 #endif
 
 DEFINE_DIALOG(DIALOG_000, 1, 6, 30, 200, _("\
-Wow! You're smack in the\n\
+Test You're smack in the\n\
 middle of the battlefield.\n\
 You'll find the Power\n\
 Stars that Bowser stole\n\
